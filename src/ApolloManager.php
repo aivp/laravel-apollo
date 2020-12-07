1abb5d7cd78dcaade480ce702d77929c4ef2cc55
linG <3137168214@qq.com>
2020-12-07 18:18:43 +0800

    更新拉取后等待时间为0秒

--- src/ApolloManager.php
+++ src/ApolloManager.php
@@ -62,7 +62,7 @@
             if ($action == 'start') {
                 $this->apollo->start($callback); //此处传入回调
             } else if ($action == 'pull') {
-                $this->apollo->setMaxLoopSeconds(10);
+                $this->apollo->setMaxLoopSeconds(0);
                 $this->apollo->start($callback); //此处传入回调
             }
         }
